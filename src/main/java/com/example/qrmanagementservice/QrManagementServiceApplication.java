53f0c77ebd39dcebd1a6c4bc03378e7f94e2c3a1
Nikita Kuritsyn <martyz89@yandex.ru>
2024-06-27 17:58:50 +0300

    1.6 Fix swagger

--- src/main/java/com/example/qrmanagementservice/QrManagementServiceApplication.java
+++ src/main/java/com/example/qrmanagementservice/QrManagementServiceApplication.java
@@ -1,5 +1,8 @@
 package com.example.qrmanagementservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +13,15 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableWebFlux
+@OpenAPIDefinition(
+        servers = {
+                @Server(
+                        url = "/qr",
+                        description = "Gateway Server Url"
+                )
+        },
+        security = @SecurityRequirement(name = "Authorization")
+)
 public class QrManagementServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/qrmanagementservice/configuration/security/SecurityConfig.java
+++ src/main/java/com/example/qrmanagementservice/configuration/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.qrmanagementservice.configuration.security;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
@@ -12,6 +15,7 @@ import org.springframework.security.web.server.context.NoOpServerSecurityContext
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 @Configuration
+@SecurityScheme(name = "Authorization", type = SecuritySchemeType.APIKEY, in = SecuritySchemeIn.HEADER)
 public class SecurityConfig {
     private static final String[] OPEN_ENDPOINTS = new String[]{
             "/actuator/**",
